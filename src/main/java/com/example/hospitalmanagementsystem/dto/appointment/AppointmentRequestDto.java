71c91134bd43a7e237759c50a348cf68ab330c31
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-16 20:27:35 +0400

    Update Dto package: AppointmentRequestDto, AppointmentResponseDtoResponseDto

--- src/main/java/com/example/hospitalmanagementsystem/dto/appointment/AppointmentRequestDto.java
+++ src/main/java/com/example/hospitalmanagementsystem/dto/appointment/AppointmentRequestDto.java
@@ -4,10 +4,13 @@ package com.example.hospitalmanagementsystem.dto.appointment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 public class AppointmentRequestDto {
-    private LocalDateTime dateTime;
+    private LocalDate appointmentDate;
+    private Long patientId;
+    private Long doctorId;
 }
--- src/main/java/com/example/hospitalmanagementsystem/dto/appointment/AppointmentResponseDto.java
+++ src/main/java/com/example/hospitalmanagementsystem/dto/appointment/AppointmentResponseDto.java
@@ -3,11 +3,18 @@ package com.example.hospitalmanagementsystem.dto.appointment;
 
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 public class AppointmentResponseDto {
 
     private Long id;
-    private LocalDateTime dateTime;
+    private LocalDate appointmentDate;
+
+    private Long patientId;
+    private String patientName;
+
+    private Long doctorId;
+    private String doctorName;
 }
