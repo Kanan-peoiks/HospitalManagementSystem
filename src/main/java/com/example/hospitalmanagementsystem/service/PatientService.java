d3e532398d7d3e7b0127467cea39332ee55f1452
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-16 23:34:30 +0400

    Update patient service

--- src/main/java/com/example/hospitalmanagementsystem/service/PatientService.java
+++ src/main/java/com/example/hospitalmanagementsystem/service/PatientService.java
@@ -1,7 +1,20 @@
 package com.example.hospitalmanagementsystem.service;
 
+import com.example.hospitalmanagementsystem.dto.patient.PatientRequestDto;
+import com.example.hospitalmanagementsystem.dto.patient.PatientResponseDto;
 import org.springframework.stereotype.Service;
 
 
 public interface PatientService {
+
+    PatientResponseDto create (PatientRequestDto patientRequestDto);
+
+    PatientResponseDto getById(Long id);
+
+    PatientResponseDto update(Long id,PatientRequestDto patientRequestDto);
+
+    PatientResponseDto deleteById(Long id);
+
+    PatientResponseDto findByName(String name);
+
 }
--- src/main/java/com/example/hospitalmanagementsystem/service/impl/PatientImplService.java
+++ src/main/java/com/example/hospitalmanagementsystem/service/impl/PatientImplService.java
@@ -1,6 +1,7 @@
 package com.example.hospitalmanagementsystem.service.impl;
 
 import com.example.hospitalmanagementsystem.dto.patient.PatientRequestDto;
+import com.example.hospitalmanagementsystem.dto.patient.PatientResponseDto;
 import com.example.hospitalmanagementsystem.entity.PatientEntity;
 import com.example.hospitalmanagementsystem.repository.PatientRepository;
 import com.example.hospitalmanagementsystem.service.AppointmentService;
@@ -11,19 +12,51 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 class PatientImplService implements PatientService {
-    private final PatientService patientService;
 
-    public PatientRequestDto createPatient(PatientRequestDto patientRequestDto) {
-        PatientEntity patient = new PatientEntity();
-        patient.setFullName(patientRequestDto.getFullName());
-        patient.setEmail(patientRequestDto.getEmail());
-        patient.setPhoneNumber(patientRequestDto.getPhoneNumber());
+    private PatientRepository patientRepository;
 
-        PatientEntity savedPatient = PatientRepository.save(patient);
-        return mapToResponseDto(savedPatient);
+    @Override
+    public PatientResponseDto create(PatientRequestDto patientRequestDto) {
+        PatientEntity patientEntity = new PatientEntity();
+
+        // melumatlari yaziriq
+        patientEntity.setFullName(patientRequestDto.getFullName());
+        patientEntity.setEmail(patientRequestDto.getEmail());
+        patientEntity.setPhoneNumber(patientRequestDto.getPhoneNumber());
+        //save edirik
+        PatientEntity saved = patientRepository.save(patientEntity);
+
+
+        PatientResponseDto patientResponseDto = new PatientResponseDto();
+
+        //daxil etdiyimiz melumatleri geri verir
+        patientResponseDto.setId(saved.getId());
+        patientResponseDto.setFullName(saved.getFullName());
+        patientResponseDto.setEmail(saved.getEmail());
+        patientResponseDto.setPhoneNumber(saved.getPhoneNumber());
+
+        return patientResponseDto;
     }
 
+    @Override
+    public PatientResponseDto getById(Long id) {
+        return null;
+    }
 
+    @Override
+    public PatientResponseDto update(Long id, PatientRequestDto patientRequestDto) {
+        return null;
+    }
+
+    @Override
+    public PatientResponseDto deleteById(Long id) {
+        return null;
+    }
+
+    @Override
+    public PatientResponseDto findByName(String name) {
+        return null;
+    }
 
 
 }
