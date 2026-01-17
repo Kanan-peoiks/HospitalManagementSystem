3e01abdfe46a1972e8b8d6289d1127756c3060c0
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-18 01:53:40 +0400

    update repository

--- src/main/java/com/example/hospitalmanagementsystem/repository/AppointmentRepository.java
+++ src/main/java/com/example/hospitalmanagementsystem/repository/AppointmentRepository.java
@@ -1,4 +1,17 @@
 package com.example.hospitalmanagementsystem.repository;
 
-public class AppointmentRepository {
+import com.example.hospitalmanagementsystem.entity.AppointmentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<AppointmentEntity,Long> {
+
+
+    @Query("select p from AppointmentEntity p  where p.patient.id = :patientId")
+    List<AppointmentEntity> findByPatientId(@Param("patientId") Long patientId);
 }
--- src/main/java/com/example/hospitalmanagementsystem/repository/DoctorRepository.java
+++ src/main/java/com/example/hospitalmanagementsystem/repository/DoctorRepository.java
@@ -1,4 +1,12 @@
 package com.example.hospitalmanagementsystem.repository;
 
-public class DoctorRepository {
+import com.example.hospitalmanagementsystem.entity.DoctorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<DoctorEntity,Long> {
+
+
+    //Query yazilacaq
 }
